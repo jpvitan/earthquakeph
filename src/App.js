e8317c3345ebfd0106a9d595a1c0e8e9c5f5af50
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-12-08 18:50:38 +0800

    Change km position

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ const EarthquakeCard = (props) => {
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-caret-down-fill" viewBox="0 0 16 16"><path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"></path></svg>
         </div>
         <div className='col-auto ps-2'>
-          <p className='mb-0' style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>{depth} km</p>
+          <p className='mb-0' style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>{depth + ' km'}</p>
         </div>
       </div>
       <div className='row'>
