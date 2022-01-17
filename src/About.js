8bcbf6f4fa3eb1694715fa958a923e721071d71d
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-01-18 07:04:29 +0800

    Remove padding

--- src/About.js
+++ src/About.js
@@ -33,7 +33,7 @@ const About = (closeWindowAction) => {
                         </div>
                     </div>
                 </div>
-                <div className='row justify-content-center text-center px-2 py-5'>
+                <div className='row justify-content-center text-center py-5'>
                     <div className='col-auto my-auto'>
                         <div className='earthquakeph-text'>earthquakeph</div>
                         <p style={{ fontWeight: 'bold' }}>Version 2.0.0</p>
