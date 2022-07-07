33e542466860300d4b91a15307256cec9a0f2fed
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-07-08 01:46:59 +0800

    Optimize about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,13 +11,13 @@ Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.
 const About = () => {
   return (
     <>
-      <div className='row justify-content-center text-center py-5'>
+      <div className='row justify-content-center text-center text-light py-5'>
         <div className='col-auto my-auto'>
           <img className='img-fluid shadow mb-2' alt='earthquakeph' src='apple-touch-icon.png' width={70} height={70} />
           <div className='earthquakeph-text'>earthquakeph</div>
-          <p style={{ fontWeight: 'bold' }}>Version 2.1.0</p>
-          <p className='mb-0' style={{ fontSize: '0.7rem', fontWeight: '500' }}>Developed and Designed by Justine Paul Sanchez Vitan.</p>
-          <p className='mb-0' style={{ fontSize: '0.7rem', fontWeight: '500' }}>Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.</p>
+          <p className='version-text'>Version 2.1.0</p>
+          <p className='mb-0 copyright-text'>Developed and Designed by Justine Paul Sanchez Vitan.</p>
+          <p className='mb-0 copyright-text'>Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.</p>
         </div>
       </div>
     </>
