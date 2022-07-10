7db4eadb176ea12e7e3abcd3c7cc5d3b70ad9e8b
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-07-10 19:36:17 +0800

    Add developer website

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,7 +17,9 @@ const About = (closeAction) => {
           <div className='earthquakeph-text'>earthquakeph</div>
           <p className='version-text'>Version 2.1.0</p>
           <p className='mb-0 copyright-text'>Developed and Designed by Justine Paul Sanchez Vitan.</p>
-          <p className='mb-0 copyright-text'>Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.</p>
+          <p className='mb-5 copyright-text'>Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.</p>
+          <img className='img-fluid shadow mb-2' alt='QR (jpvitan.com)' src='img/qr (jpvitan.com).png' width={100} height={100} />
+          <a href="https://jpvitan.com/" className='developer-website-text'><p>Developer Website</p></a>
         </div>
       </div>
     </>
