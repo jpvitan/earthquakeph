05b48eca4c92ad2ce679e6b6fcaea2298de91dba
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-07-13 02:41:32 +0800

    Design loading screen

--- src/App.js
+++ src/App.js
@@ -182,7 +182,18 @@ const LoadingScreen = () => {
   return (
     <>
       {visible &&
-        <div className='w-100 h-100 loading-screen' />}
+        <div className='w-100 h-100 loading-screen'>
+          <div className='container h-100'>
+            <div className='row justify-content-center h-100'>
+              <div className='col-auto my-auto text-center text-light'>
+                <img className='img-fluid shadow mb-4' alt='earthquakeph' src='apple-touch-icon.png' width={70} height={70} />
+                <div id='spinner_container' className='d-flex justify-content-center mb-5'>
+                  <div className='spinner-border text-danger' role='status' />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>}
     </>
   )
 }
