bc1f7fb49d00cf8c96e84de0d79160215f9a7fbf
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-07-13 03:37:12 +0800

    Change QR design

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,7 +18,7 @@ const About = (closeAction) => {
           <p className='version-text'>Version 2.1.0</p>
           <p className='mb-0 copyright-text'>Developed and Designed by Justine Paul Sanchez Vitan.</p>
           <p className='mb-5 copyright-text'>Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.</p>
-          <img className='img-fluid shadow mb-2' alt='QR (jpvitan.com)' src='img/qr (jpvitan.com).png' width={100} height={100} />
+          <img className='img-fluid shadow mb-2' alt='QR (jpvitan.com)' src='img/qr (jpvitan.com).png' width={150} height={150} />
           <a href='https://jpvitan.com/' className='developer-website-text'><p>Developer Website</p></a>
         </div>
       </div>
