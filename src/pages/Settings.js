ba02cdbe85fada793478606ef463963e817e2f77
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-07-14 02:04:10 +0800

    Add license notice on code

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -3,6 +3,7 @@
 earthquakeph
 Real-time app that detects the latest earthquake recorded by the USGS within the Philippines.
 
+LICENSE: MIT License
 Created by Justine Paul Sanchez Vitan.
 Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.
 
