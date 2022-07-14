e97b0073033a54a22e2f39d2a4f477256620d49e
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-07-15 00:02:36 +0800

    Bump version number

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,7 +16,7 @@ const About = (closeAction) => {
         <div className='col my-auto' style={{ maxWidth: '35rem' }}>
           <img className='img-fluid shadow mb-2' alt='earthquakeph' src='apple-touch-icon.png' width={70} height={70} />
           <div className='earthquakeph-text'>earthquakeph</div>
-          <p className='version-text'>Version 2.1.1</p>
+          <p className='version-text'>Version 2.1.2</p>
           <p className='mb-2 copyright-text'>This project is under the <a href='https://github.com/jpvitan/earthquakeph/blob/master/LICENSE'>MIT license</a>. Please read the terms and conditions stated within the license before attempting any modification or distribution of the software.</p>
           <p className='mb-0 copyright-text'>Developed and Designed by <a href='https://jpvitan.com/'>Justine Paul Sanchez Vitan</a>.</p>
           <p className='mb-4 copyright-text'>Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.</p>
