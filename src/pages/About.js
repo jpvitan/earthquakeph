c75e29c356f0f0a6237c6efd4385f70a473dad09
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-07-23 02:37:41 +0800

    Remove PWA Shields

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,7 +17,6 @@ const About = (closeAction) => {
           <img className='img-fluid shadow mb-2' alt='earthquakeph' src='apple-touch-icon.png' width={70} height={70} />
           <div className='earthquakeph-text'>earthquakeph</div>
           <p className='version-text'>Version 2.1.2</p>
-          <img className='mb-3' alt='PWA Shields' src='https://www.pwa-shields.com/1.0.0/series/classic/white/gray.svg' width={75} height={20} />
           <p className='mb-2 copyright-text'>This project is under the <a href='https://github.com/jpvitan/earthquakeph/blob/master/LICENSE'>MIT license</a>. Please read the terms and conditions stated within the license before attempting any modification or distribution of the software.</p>
           <p className='mb-0 copyright-text'>Developed and Designed by <a href='https://jpvitan.com/'>Justine Paul Sanchez Vitan</a>.</p>
           <p className='mb-4 copyright-text'>Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.</p>
