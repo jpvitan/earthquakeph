6d7bb2a5c2f3f4c9cccbc48803a2bdb730f258cd
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-11-23 05:01:14 +0800

    Move bootstrap import

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
