ec9e532d272af53c15f61e2d0a6d155a2ff25349
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-12-15 01:24:26 +0800

    Restructure History.js

--- src/pages/History.js
+++ src/pages/History.js
@@ -9,10 +9,34 @@ Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.
 
 */
 
+import Utility from '../utility/Utility'
+
 const History = (onClose, globalProperties) => {
+  const { earthquake } = globalProperties
+
   return (
-    <>
-    </>
+    <div id='history_container'>
+      {
+        earthquake.list.map((earthquake) => {
+          return (
+            <div className='row px-2 pb-5' key={earthquake.id}>
+              <div
+                className='col-auto my-auto text-center' style={{ cursor: 'pointer' }} onClick={() => {
+                  onClose()
+                }}
+              >
+                <h1 style={{ fontWeight: 'bold', color: Utility.getMagnitudeColor(earthquake.magnitude) }}>{earthquake.magnitude.toFixed(1)}</h1>
+                <p className='depth-paragraph badge bg-warning mb-0'>{earthquake.depth + ' km'}</p>
+              </div>
+              <div className='col my-auto'>
+                <p className='location-paragraph mb-0'>{earthquake.location}</p>
+                <p className='time-paragraph mb-0'>{new Date(earthquake.time).toLocaleString()}</p>
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
