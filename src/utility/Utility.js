93b3947d8eaeaa435eb8617231585817cbb701ca
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-12-27 14:23:43 +0800

    Restructure Utility.js

--- src/utility/Utility.js
+++ src/utility/Utility.js
@@ -13,105 +13,61 @@ import Configuration from './Configuration'
 import DataCycle from './DataCycle'
 
 export default class Utility {
-  static configuration = new Configuration(1, 1, 10, 30, 900, 'theme-black-pearl', 'mapbox://styles/jpvitan/ckwjznqa44qhz14qnswqs0koo')
+  /* Configuration */
+  static configuration = new Configuration('Philippines', 1, 10, 30, 180, 'Black Pearl', 'Dark')
+  /* DataCycle */
   static dataCycle = new DataCycle(this.configuration)
-  static display = { toggleLoadingVisibility: () => { }, toggleMessageScreen: () => { }, setIndicatorColor: () => { } }
+
+  /* Display */
+  static display = {
+    toggleLoadingVisibility: () => {
+
+    },
+    toggleMessageScreen: () => {
+
+    },
+    setIndicatorColor: () => {
+
+    },
+    setWebsiteTint: (color) => {
+      const element = document.querySelector('meta[name="theme-color"]')
+      if (element) element.setAttribute('content', color)
+    }
+  }
+
+  /* Map */
   static map = { setCoordinates: () => { } }
 
-  static cycleLocation () {
-    const locations = this.getLocations()
-    let index = this.configuration.squareAreaValue
-    index++
-    if (index > locations.length - 1) index = 0
-    this.configuration.squareAreaValue = index
-    this.dataCycle.update()
-  }
-
-  static cycleTheme () {
-    const themes = this.getThemes()
-    let index = themes.findIndex((theme) => theme === this.configuration.theme)
-    index++
-    if (index > themes.length - 1) index = 0
-    this.configuration.setTheme(themes[index])
-  }
-
-  static cycleMapTheme () {
-    const mapThemes = this.getMapThemes()
-    let index = mapThemes.findIndex((mapTheme) => mapTheme === this.configuration.mapTheme)
-    index++
-    if (index > mapThemes.length - 1) index = 0
-    this.configuration.mapTheme = mapThemes[index]
-    this.dataCycle.update(true)
-  }
-
-  static getLocations () {
-    return ['All', 'Philippines']
-  }
-
-  static getThemes () {
-    return ['theme-black-pearl', 'theme-deep-black', 'theme-shadowed-steel', 'theme-total-eclipse']
-  }
-
-  static getMapThemes () {
-    return ['mapbox://styles/jpvitan/ckwjznqa44qhz14qnswqs0koo', 'mapbox://styles/darkaxe201/ckhuud56s00xw1as9bnzdupdw', 'mapbox://styles/darkaxe201/ckhupcwep3gh31apgealmhkdc']
-  }
-
-  static getStatusColor (status) {
-    let statusColor = '#95a5a6'
-
-    if (status === 'success') {
-      statusColor = '#2ecc71'
-    } else if (status === 'fetching') {
-      statusColor = '#f39c12'
-    } else if (status === 'error') {
-      statusColor = '#e74c3c'
+  /* Status */
+  static status = {
+    getColor: (status) => {
+      let color = '#95a5a6'
+      if (status === 'success') color = '#2ecc71'
+      else if (status === 'fetching') color = '#f39c12'
+      else if (status === 'error') color = '#e74c3c'
+      return color
     }
-
-    return statusColor
   }
 
-  static getMagnitudeColor (magnitude) {
-    let magnitudeColor = '#e74c3c'
-
-    if (magnitude >= 1 && magnitude <= 3.9) {
-      magnitudeColor = '#7f8c8d'
-    } else if (magnitude >= 4 && magnitude <= 4.9) {
-      magnitudeColor = '#f1c40f'
-    } else if (magnitude >= 5 && magnitude <= 5.9) {
-      magnitudeColor = '#f39c12'
-    } else if (magnitude >= 6 && magnitude <= 6.9) {
-      magnitudeColor = '#d35400'
-    } else if (magnitude >= 7 && magnitude <= 7.9) {
-      magnitudeColor = '#c0392b'
-    } else if (magnitude >= 8) {
-      magnitudeColor = '#9b59b6'
+  /* Magnitude */
+  static magnitude = {
+    getColor: (magnitude) => {
+      let color = '#e74c3c'
+      if (magnitude >= 1 && magnitude <= 3.9) color = '#7f8c8d'
+      else if (magnitude >= 4 && magnitude <= 4.9) color = '#f1c40f'
+      else if (magnitude >= 5 && magnitude <= 5.9) color = '#f39c12'
+      else if (magnitude >= 6 && magnitude <= 6.9) color = '#d35400'
+      else if (magnitude >= 7 && magnitude <= 7.9) color = '#c0392b'
+      else if (magnitude >= 8) color = '#9b59b6'
+      return color
+    },
+    createCircle: (earthquake) => {
+      const circle = document.createElement('div')
+      circle.id = `magnitude-circle-${earthquake.id}`
+      circle.className = 'magnitude-circle'
+      circle.style.backgroundColor = this.getMagnitudeColor(earthquake.magnitude)
+      circle.innerHTML = `<div>${Math.floor(earthquake.magnitude)}</div>`
+      return circle
     }
-
-    return magnitudeColor
-  }
-
-  static getMagnitudeArrayBounds (minMagnitude, maxMagnitude) {
-    const minMagnitudeArray = []
-    const maxMagnitudeArray = []
-
-    for (let i = 1; i <= maxMagnitude - 1; i++) {
-      minMagnitudeArray.push(i)
-    }
-    for (let j = minMagnitude + 1; j <= 10; j++) {
-      maxMagnitudeArray.push(j)
-    }
-
-    return [minMagnitudeArray, maxMagnitudeArray]
-  }
-
-  static createMagnitudeCircle (earthquake) {
-    const magnitudeCircle = document.createElement('div')
-
-    magnitudeCircle.id = 'magnitude-circle-' + earthquake.id
-    magnitudeCircle.className = 'magnitude-circle'
-    magnitudeCircle.style.backgroundColor = this.getMagnitudeColor(earthquake.magnitude)
-    magnitudeCircle.innerHTML = '<div>' + Math.floor(earthquake.magnitude) + '</div>'
-
-    return magnitudeCircle
   }
 }
