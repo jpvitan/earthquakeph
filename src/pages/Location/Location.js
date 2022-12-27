733620d1161c5e7317b1169e94cce072b3315008
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-12-27 17:18:08 +0800

    Create location page

---
+++ src/pages/Location/Location.js
@@ -0,0 +1,20 @@
+/*
+
+earthquakeph
+Real-time app that detects the latest earthquake recorded by the USGS within the Philippines.
+
+LICENSE: MIT License
+Created by Justine Paul Sanchez Vitan.
+Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.
+
+*/
+
+import './Location.scss'
+
+const Location = ({ onClose, earthquake }) => {
+  return (
+    <div className='location' />
+  )
+}
+
+export default Location
