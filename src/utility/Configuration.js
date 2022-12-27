27208b593dd6143d4b70f16d68e5f4db48a829d6
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-12-27 12:13:45 +0800

    Restructure Configuration.js

--- src/utility/Configuration.js
+++ src/utility/Configuration.js
@@ -9,22 +9,60 @@ Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.
 
 */
 
+import Location from '../data/Location'
+import AppTheme from '../data/AppTheme'
+import MapTheme from '../data/MapTheme'
+
 export default class Configuration {
-  constructor (squareAreaValue, minMagnitude, maxMagnitude, plot, updateInterval, theme, mapTheme) {
-    this.squareAreaValue = squareAreaValue
+  constructor (location, minMagnitude, maxMagnitude, plot, interval, appTheme, mapTheme) {
+    this.location = location
     this.minMagnitude = minMagnitude
     this.maxMagnitude = maxMagnitude
     this.plot = plot
-    this.updateInterval = updateInterval
-    this.theme = theme
+    this.interval = interval
+    this.appTheme = appTheme
     this.mapTheme = mapTheme
   }
 
-  setTheme (theme) {
-    this.theme = theme
+  getLocation () {
+    return Location.find((location) => location.name === this.location)
+  }
+
+  getAppTheme () {
+    return AppTheme.find((appTheme) => appTheme.name === this.appTheme)
+  }
+
+  getMapTheme () {
+    return MapTheme.find((mapTheme) => mapTheme.name === this.mapTheme)
+  }
+
+  setAppTheme (appTheme) {
+    this.appTheme = appTheme
+    const { className, color } = this.getAppTheme()
     const app = document.getElementById('app')
-    if (app) {
-      app.className = theme
-    }
+    const element = document.querySelector('meta[name="theme-color"]')
+    if (app) app.className = className
+    if (element) element.setAttribute('content', color)
+  }
+
+  setNextLocation () {
+    let index = Location.findIndex((location) => location.name === this.location)
+    index++
+    if (index > Location.length - 1) index = 0
+    this.location = Location[index].name
+  }
+
+  setNextAppTheme () {
+    let index = AppTheme.findIndex((appTheme) => appTheme.name === this.appTheme)
+    index++
+    if (index > AppTheme.length - 1) index = 0
+    this.setAppTheme(AppTheme[index].name)
+  }
+
+  setNextMapTheme () {
+    let index = MapTheme.findIndex((mapTheme) => mapTheme.name === this.mapTheme)
+    index++
+    if (index > MapTheme.length - 1) index = 0
+    this.mapTheme = MapTheme[index].name
   }
 }
