52ba3e6ab3a0ea57381804056528148dcf721b19
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-12-29 11:19:41 +0800

    Display location data

--- src/pages/Location/Location.js
+++ src/pages/Location/Location.js
@@ -9,11 +9,37 @@ Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.
 
 */
 
+import LocationData from '../../data/Location'
 import './Location.scss'
 
 const Location = ({ onClose, earthquake }) => {
   return (
-    <div className='location' />
+    <div className='location'>
+      <div className='container-fluid px-0'>
+        {
+          LocationData.map((location) => {
+            const { name, area, code } = location
+
+            const handleOnClick = () => {
+              onClose()
+            }
+
+            return (
+              <div key={code} className='row mb-5' style={{ cursor: 'pointer' }} onClick={handleOnClick}>
+                <div className='col-auto my-auto'>
+                  <div className='code-circle'>
+                    <p>{code}</p>
+                  </div>
+                </div>
+                <div className='col my-auto'>
+                  <p className='name-paragraph mb-0'>{name}</p>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
   )
 }
 
