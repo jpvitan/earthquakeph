9f0f433066b31c80d0ac0476f1baac93b66a306d
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-03-03 00:56:42 +0800

    Reset pages

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -13,52 +13,9 @@ Developer's Website: https://jpvitan.com/
 
 */
 
-import Notice from '../../components/Notice/Notice'
-import Icon from '../../utility/Icon'
-import Utility from '../../utility/Utility'
-
 const History = ({ onClose, earthquake }) => {
   return (
-    <div className='history'>
-      <div className='container-fluid px-0'>
-        <PreContent />
-        {earthquake.list.map((earthquake) => <Unit key={earthquake.id} {...earthquake} onClose={onClose} />)}
-      </div>
-    </div>
-  )
-}
-
-const PreContent = () => {
-  return (
-    <div className='row mb-2'>
-      <div className='col'>
-        <Notice data={[
-          { id: 1, icon: Icon.About(), message: 'You might see some results from adjacent or neighboring countries due to overlapping bounding boxes. This behavior is normal and expected.' }
-        ]}
-        />
-      </div>
-    </div>
-  )
-}
-
-const Unit = ({ id, longitude, latitude, magnitude, depth, location, time, onClose }) => {
-  const handleOnClick = () => {
-    Utility.map.setCoordinates(longitude, latitude, 10)
-    onClose()
-  }
-
-  return (
-    <div className='unit row mb-5' onClick={handleOnClick}>
-      <div className='col-auto my-auto'>
-        <div className='magnitude-square shadow-lg' style={{ backgroundColor: Utility.magnitude.getColor(magnitude) }}>
-          <p>{magnitude.toFixed(1)}</p>
-        </div>
-      </div>
-      <div className='col my-auto'>
-        <p className='location-paragraph mb-0'>{location}</p>
-        <p className='time-paragraph mb-0'>{new Date(time).toLocaleString('en-US', { hour12: false })}</p>
-      </div>
-    </div>
+    <></>
   )
 }
 
--- src/pages/Location/Location.js
+++ src/pages/Location/Location.js
@@ -13,58 +13,9 @@ Developer's Website: https://jpvitan.com/
 
 */
 
-import Notice from '../../components/Notice/Notice'
-import Icon from '../../utility/Icon'
-import Utility from '../../utility/Utility'
-import LocationData from '../../data/Location'
-
 const Location = ({ onClose, earthquake }) => {
   return (
-    <div className='location'>
-      <div className='container-fluid px-0'>
-        <PreContent />
-        {LocationData.map((location) => <Unit key={location.code} {...location} onClose={onClose} />)}
-      </div>
-    </div>
-  )
-}
-
-const PreContent = () => {
-  return (
-    <div className='row mb-2'>
-      <div className='col'>
-        <Notice data={[
-          { id: 1, icon: Icon.About(), message: 'You might see some results from adjacent or neighboring countries due to overlapping bounding boxes. This behavior is normal and expected.' },
-          { id: 2, icon: Icon.PatchCheck({ color: '#2ecc71' }), message: 'Results may be less accurate for countries without a verified bounding box.' }
-        ]}
-        />
-      </div>
-    </div>
-  )
-}
-
-const Unit = ({ name, code, verified, onClose }) => {
-  const handleOnClick = () => {
-    onClose()
-    Utility.display.toggleLoadingVisibility(true)
-    Utility.configuration.location = name
-    Utility.dataCycle.update()
-  }
-
-  return (
-    <div className='unit row mb-5' onClick={handleOnClick}>
-      <div className='col-auto my-auto'>
-        <div className='code-circle shadow-lg'>
-          <p>{code}</p>
-        </div>
-      </div>
-      <div className='col-auto my-auto'>
-        <p className='name-paragraph mb-0'>{name}</p>
-      </div>
-      <div className='col-auto my-auto px-0'>
-        {verified && Icon.PatchCheck({ display: 'block', width: '15px', height: '15px', color: '#2ecc71' })}
-      </div>
-    </div>
+    <></>
   )
 }
 
