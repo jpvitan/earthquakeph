ce14860d9c65346ec8eee1189cd34d8d11655c75
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-03-03 12:38:58 +0800

    Fix information card layout

--- src/components/Earthquake.js
+++ src/components/Earthquake.js
@@ -36,39 +36,37 @@ const InformationCard = ({ longitude, latitude, location, depth, magnitude }) =>
   const handleOnClick = () => Utility.map.setCoordinates(longitude, latitude, Utility.configuration.zoom)
 
   return (
-    <div className='card-information shadow-lg text-light' onClick={handleOnClick}>
-      <div className='container-fluid px-4 py-4'>
-        <div className='row mb-2'>
-          <div className='col-auto'>
-            <h1 className='my-0' style={{ fontWeight: 'bold', color: Utility.magnitude.getColor(magnitude) }}>{magnitude.toFixed(1)}</h1>
-          </div>
-          <div className='col-auto my-auto px-0'>
-            {magnitude >= 6 && <img className='indicator-warning' src={warningIndicator} alt='Warning Indicator' width={30} height={30} />}
-          </div>
+    <div className='card-information shadow-lg px-4 py-4' onClick={handleOnClick}>
+      <div className='row g-0'>
+        <div className='col-auto my-auto pe-2'>
+          <p className='text-size-figure fw-bold mb-0' style={{ color: Utility.magnitude.getColor(magnitude) }}>{magnitude.toFixed(1)}</p>
         </div>
-        <div className='row'>
-          <div className='col-auto pe-0'>
-            {Icon.Down({ width: 18, height: 18 })}
-          </div>
-          <div className='col-auto ps-2'>
-            <p className='text-size-lg fw-bold mb-0'>{depth + ' km'}</p>
-          </div>
-          <div className='col-auto my-auto px-0'>
-            <FetchIndicator />
-          </div>
+        <div className='col-auto my-auto pe-2'>
+          {magnitude >= 6 && <img className='indicator-warning' src={warningIndicator} alt='Warning Indicator' width={30} height={30} />}
         </div>
-        <div className='row'>
-          <div className='col'>
-            <p className='text-size-lg fw-bold mb-0'>{location}</p>
-          </div>
+      </div>
+      <div className='row g-0'>
+        <div className='col-auto my-auto pe-2'>
+          {Icon.Down({ display: 'block', width: 18, height: 18, color: '#fff' })}
         </div>
-        <div className='row'>
-          <div className='col-auto'>
-            <MagnitudeScale />
-          </div>
-          <div className='col-auto'>
-            <LocationIndicator />
-          </div>
+        <div className='col-auto my-auto pe-2'>
+          <p className='text-size-lg fw-bold mb-0'>{depth + ' km'}</p>
+        </div>
+        <div className='col-auto my-auto pe-2'>
+          <FetchIndicator />
+        </div>
+      </div>
+      <div className='row g-0'>
+        <div className='col my-auto'>
+          <p className='text-size-lg fw-bold mb-0'>{location}</p>
+        </div>
+      </div>
+      <div className='row g-0 mt-2'>
+        <div className='col-auto my-auto'>
+          <MagnitudeScale />
+        </div>
+        <div className='col-auto my-auto'>
+          <LocationIndicator />
         </div>
       </div>
     </div>
@@ -89,17 +87,13 @@ const FetchIndicator = () => {
 
 const MagnitudeScale = () => {
   return (
-    <div>
-      <div className='container-fluid px-0'>
-        <div className='row'>
-          <ScaleUnit value={3} color={Utility.magnitude.getColor(3)} text='3-' />
-          <ScaleUnit value={4} color={Utility.magnitude.getColor(4)} text='4' />
-          <ScaleUnit value={5} color={Utility.magnitude.getColor(5)} text='5' />
-          <ScaleUnit value={6} color={Utility.magnitude.getColor(6)} text='6' />
-          <ScaleUnit value={7} color={Utility.magnitude.getColor(7)} text='7' />
-          <ScaleUnit value={8} color={Utility.magnitude.getColor(8)} text='8+' />
-        </div>
-      </div>
+    <div className='row g-0'>
+      <ScaleUnit value={3} color={Utility.magnitude.getColor(3)} text='3-' />
+      <ScaleUnit value={4} color={Utility.magnitude.getColor(4)} text='4' />
+      <ScaleUnit value={5} color={Utility.magnitude.getColor(5)} text='5' />
+      <ScaleUnit value={6} color={Utility.magnitude.getColor(6)} text='6' />
+      <ScaleUnit value={7} color={Utility.magnitude.getColor(7)} text='7' />
+      <ScaleUnit value={8} color={Utility.magnitude.getColor(8)} text='8+' />
     </div>
   )
 }
@@ -111,7 +105,7 @@ const ScaleUnit = ({ value, color, text }) => {
   }
 
   return (
-    <div className='col-auto pe-1 mt-2'>
+    <div className='col-auto pe-3 my-auto'>
       <div className='indicator-scale d-flex justify-content-center align-items-center' style={{ backgroundColor: color }} onClick={handleOnClick}>
         <p className='text-size-sm fw-bold mb-0'>{text}</p>
       </div>
@@ -121,7 +115,7 @@ const ScaleUnit = ({ value, color, text }) => {
 
 const LocationIndicator = () => {
   return (
-    <div className='indicator-location d-flex justify-content-center align-items-center mt-2'>
+    <div className='indicator-location d-flex justify-content-center align-items-center'>
       <p className='text-size-xs fw-bold mb-0'>{Utility.configuration.getLocation().code}</p>
     </div>
   )
