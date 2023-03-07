0331a17dd8458ea4de1e32a0d3270e0f13cbe73d
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-03-08 04:26:50 +0800

    Update source code notice on utility files

--- src/js/utilities/Utility.js
+++ src/js/utilities/Utility.js
@@ -1,7 +1,7 @@
 /*
 
-earthquakeph
-A highly customizable real-time web application that tracks the latest earthquake recorded by the USGS within the Philippines and the world.
+EarthquakePH
+A highly customizable real-time and progressive web application that tracks and monitors the latest earthquake recorded by the United States Geological Survey within the Philippines and the world.
 
 This project is under the MIT license.
 Please read the terms and conditions stated within the license before attempting any modification or distribution of the software.
