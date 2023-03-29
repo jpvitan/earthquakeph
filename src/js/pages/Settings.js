fd03edc5ac17f91af987bae6a3cfb804829e70b3
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-03-30 07:58:40 +0800

    Setup form in settings

--- src/js/pages/Settings.js
+++ src/js/pages/Settings.js
@@ -13,9 +13,35 @@ Developer's Website: https://jpvitan.com/
 
 */
 
+import { useState } from 'react'
+
 const Settings = ({ configuration, engine, earthquake, onClose }) => {
   return (
-    <></>
+    <div className='settings'>
+      <Form configuration={configuration} engine={engine} onClose={onClose} />
+    </div>
+  )
+}
+
+const Form = ({ configuration, engine, onClose }) => {
+  const [interval, setInterval] = useState(configuration.interval)
+  const [zoom, setZoom] = useState(((configuration.zoom - 3) / 19 * 100).toFixed(0))
+  const [showBoundingBox, setShowBoundingBox] = useState(configuration.showBoundingBox)
+  const [plot, setPlot] = useState(configuration.plot)
+
+  const submit = (e) => {
+    e.preventDefault()
+    if (configuration.toggleLoading) configuration.toggleLoading(true)
+    configuration.interval = interval
+    configuration.zoom = 3 + (19 * (zoom / 100))
+    configuration.showBoundingBox = showBoundingBox
+    configuration.plot = plot
+    engine.update()
+    onClose()
+  }
+
+  return (
+    <form onSubmit={submit} />
   )
 }
 
