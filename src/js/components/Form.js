0c450bcead86a0f680398fcba3660cadb41deb8b
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-03-30 08:14:22 +0800

    Add Form.js

---
+++ src/js/components/Form.js
@@ -0,0 +1,14 @@
+/*
+
+EarthquakePH
+A highly customizable real-time and progressive web application that tracks and monitors the latest earthquake recorded by the United States Geological Survey within the Philippines and the world.
+
+This project is under the MIT license.
+Please read the terms and conditions stated within the license before attempting any modification or distribution of the software.
+
+Copyright © 2022 Justine Paul Vitan. All rights reserved.
+
+License Information: https://github.com/jpvitan/earthquakeph/blob/master/LICENSE
+Developer's Website: https://jpvitan.com/
+
+*/
