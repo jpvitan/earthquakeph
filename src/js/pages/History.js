90ed2627653949067b9806eb264bf7d9501f0b47
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-04-28 01:58:52 +0800

    Add history search field

--- src/js/pages/History.js
+++ src/js/pages/History.js
@@ -13,9 +13,13 @@ Developer's Website: https://jpvitan.com/
 
 */
 
+import { Field } from '../components/Form'
 import Icon from '../utilities/Icon'
+import { useState } from 'react'
 
 const History = ({ configuration, engine, earthquake, onClose }) => {
+  const [search, setSearch] = useState('')
+
   const focus = (earthquake) => {
     if (configuration.app.map) configuration.app.map.flyTo({ center: [earthquake.longitude, earthquake.latitude], zoom: 12 })
     onClose()
@@ -23,12 +27,14 @@ const History = ({ configuration, engine, earthquake, onClose }) => {
 
   return (
     <div className='history'>
-      <div className='row mt-3 mb-5'>
-        <div className='col-auto my-auto'>
-          {Icon.Globe({ display: 'block', width: 20, height: 20, color: '#fff' })}
-        </div>
+      <div className='row mt-3'>
         <div className='col my-auto'>
-          <p className='mb-0'>You might see some results from adjacent or neighboring countries due to overlapping bounding boxes. This behavior is normal and expected.</p>
+          <Field label={Icon.Search({ width: 15, height: 15, color: '#999' })} placeholder='Search' value={search} onChange={(e) => { setSearch(e.target.value) }} />
+        </div>
+      </div>
+      <div className='row mt-3 mb-4'>
+        <div className='col my-auto'>
+          <p className='text-size-sm mb-0'>You might see some results from adjacent or neighboring countries due to overlapping bounding boxes. This behavior is normal and expected.</p>
         </div>
       </div>
       {earthquake.list.map((earthquake) => <Unit key={earthquake.id} earthquake={earthquake} onClick={() => { focus(earthquake) }} />)}
