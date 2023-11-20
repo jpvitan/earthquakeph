84593591b2b8c53a8bf34be05e1916a5d6443c16
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-11-21 07:18:55 +0800

    Add marker component

--- src/js/pages/System.js
+++ src/js/pages/System.js
@@ -22,6 +22,11 @@ const System = ({ configuration, engine, earthquake, onClose }) => {
       <div className='row justify-content-center'>
         <div className='content-xs col'>
           <section className='mt-5'>
+            <BoardStack>
+              <Marker />
+            </BoardStack>
+          </section>
+          <section className='mt-5'>
             <p className='text-size-md fw-bold'>Links</p>
             <BoardStack>
               <Link
@@ -42,6 +47,18 @@ const System = ({ configuration, engine, earthquake, onClose }) => {
           </section>
         </div>
       </div>
+    </div>
+  )
+}
+
+const Marker = () => {
+  return (
+    <div className='marker text-center'>
+      <img className='shadow-lg mb-3' alt='EarthquakePH' src='apple-touch-icon.png' width={25} height={25} />
+      <p className='text-size-xs'><strong>EarthquakePH</strong> is a highly customizable real-time and progressive web application that tracks and monitors the latest earthquake recorded by the United States Geological Survey within the Philippines and the world.</p>
+      <p className='text-size-xs'>Built by <strong>Justine Paul Vitan</strong> as a solo project to demonstrate his capabilities in developing rich and scalable web applications. The source code of this project is open and available to the public via GitHub for transparency and open-source collaboration.</p>
+      <p className='text-size-xs mb-0'>Developed and Designed by Justine Paul Vitan.</p>
+      <p className='text-size-xs mb-0'>Copyright © 2022 Justine Paul Vitan. All rights reserved.</p>
     </div>
   )
 }
